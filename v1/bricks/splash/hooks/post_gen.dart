2cc92c1ef418eaed311fe6e6a844549a5223fe88
baer-tech <boghedian@gmail.com>
2022-11-25 01:03:25 +0200

    Add post gen hook to splash

---
+++ v1/bricks/splash/hooks/post_gen.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+import 'package:mason/mason.dart';
+import 'package:yaml_modify/yaml_modify.dart';
+
+Future run(HookContext context) async {
+  final logger = context.logger;
+
+  final directory = Directory.current.path;
+
+  try {
+    File file = File("pubspec.yaml");
+
+    final yaml = loadYaml(file.readAsStringSync());
+
+    final modifiable = getModifiableNode(yaml);
+    modifiable['dependencies'] = {
+      'splash': {'path': 'features/splash'}
+    };
+
+    final strYaml = toYamlString(modifiable);
+    File("pubspec.yaml").writeAsStringSync(strYaml);
+  } on FileSystemException catch (_) {
+    logger.alert(
+      red.wrap(
+        'Could not find pubspec.yaml',
+      ),
+    );
+
+    throw Exception();
+  } on Exception catch (e) {
+    throw e;
+  }
+}
